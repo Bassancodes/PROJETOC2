004106396d22ae2d6fcf3b9887264b3c7fc529dc
henrique bassan rebechi <145149804+Bassancodes@users.noreply.github.com>
2024-10-25 16:21:24 +0000

    adicionando as funcoes para dar inicio ao codigo no projeto.c

--- projeto.c
+++ projeto.c
@@ -2,4 +2,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int 
+void cadastrarUsuario(){
+    printf("Cadastro");
+    return 0;
+}
+
+int loginUsuario(){
+    printf("login");
+    return 0;
+}
+
+void trocarSenha(){
+    printf("trocarsenha");
+    return 0;
+}
+
+void removerConta(){
+    printf("remover");
+    return 0;
+}
+
+void enviarSolicitacaoAmizade{
+    printf("solicitação");
+    return 0;
+}
