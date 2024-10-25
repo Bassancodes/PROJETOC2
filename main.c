499e2cd66e5b7aaf42572571c9bf122c84eb8743
henrique bassan rebechi <145149804+Bassancodes@users.noreply.github.com>
2024-10-25 17:39:13 +0000

    arrumando a main nos ultimos detalhes

--- main.c
+++ main.c
@@ -1,37 +1,21 @@
-#include <stdio.h>
-#include "projeto.h"
+#ifndef PROJETO_H
+#define PROJETO_H
 
-int main() {
-    int opcao;
-    Usuario usuarioLogado;
+typedef struct {
+    int id;
+    char nome[50];
+    char cpf[12];
+    char senha[20];
+    int amigos[10];
+    int totalAmigos;
+} Usuario;
 
-    do {
-        printf("\n--- Sistema de Cadastro de Usuarios ---\n");
-        printf("1. Cadastrar Usuario\n");
-        printf("2. Entrar\n");
-        printf("3. Sair\n");
-        printf("Escolha uma opcao: ");
-        scanf("%d", &opcao);
 
-        switch (opcao) {
-            case 1:
-                cadastrarUsuario();
-                break;
-            case 2:
-                if (loginUsuario(&usuarioLogado)) {
-                    printf("Login realizado com sucesso!\n");
-                    menuUsuario(&usuarioLogado);
-                } else {
-                    printf("CPF ou senha incorretos!\n");
-                }
-                break;
-            case 3:
-                printf("Saindo...\n");
-                break;
-            default:
-                printf("Opcao invalida!\n");
-        }
-    } while (opcao != 3);
+void cadastrarUsuario();
+int loginUsuario(Usuario *usuarioLogado);
+void menuUsuario(Usuario *usuarioLogado);
+void trocarSenha(Usuario *usuarioLogado);
+void removerConta(char *cpf);
+void enviarSolicitacaoAmizade(Usuario *usuarioLogado);
 
-    return 0;
-}
+#endif 
